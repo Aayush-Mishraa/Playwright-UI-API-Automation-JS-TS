f0a14333268f912976dd0b9359a74c27e0051e95
Aayush Mishra <aayushmishra026@gmail.com>
2026-02-09 20:03:30 +0530

    Fix some code

--- tests/ClientAPP.spec.js
+++ tests/ClientAPP.spec.js
@@ -12,13 +12,13 @@ test.only("Login into new Applcation", async ({ page }) => {
     await password.fill("Iamking@000");
     await loginButton.click();
     await page.waitForLoadState('networkidle');//this method waits for the network to be idle, which means that there are no more network requests being made. This is useful to ensure that the page has fully loaded before proceeding with the test.
-    
+
 
     // await page.locator(".card-body b").first().waitFor();
     // console.log(page.locator(".card-body b").first());
     console.log(await page.locator(".card-body b").allTextContents());
 
-
+  console.log(await page.locator(".card-body b").allTextContents());
 
 
 
